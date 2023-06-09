56da9e90c3afd81497edd760db1a027e52f31de1
kodxana <romanrex19@int.pl>
2023-06-09 13:58:04 +0200

    Create model_fetcher.py

---
+++ builder/model_fetcher.py
@@ -0,0 +1,18 @@
+# builder/model_fetcher.py
+
+import torch
+from diffusers import DiffusionPipeline
+
+def get_kandinsky_pipelines():
+    pipe_prior = DiffusionPipeline.from_pretrained("kandinsky-community/kandinsky-2-1-prior", 
+                                                   torch_dtype=torch.float16)
+    pipe_prior.to("cuda")
+
+    t2i_pipe = DiffusionPipeline.from_pretrained("kandinsky-community/kandinsky-2-1", 
+                                                 torch_dtype=torch.float16)
+    t2i_pipe.to("cuda")
+
+    return pipe_prior, t2i_pipe
+
+if __name__ == "__main__":
+    get_kandinsky_pipelines()
